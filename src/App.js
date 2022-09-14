1f5904c0f16380f96348a3ecefa94423ffeebf8b
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-13 23:50:29 -0500

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from './components/Header';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Header = () => {
+  return (
+    <Navbar sticky='top' className='navbar justify-content-center'>
+        <Nav className='mt-5 mb-5 nav' activeKey='/home' variant='pills'>
+            <Nav.Item>
+                <Nav.Link href='/home'>Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey='link-1'>About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey='link-2'>Shop</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey='link-2'>Contact</Nav.Link>
+            </Nav.Item>
+        </Nav>
+    </Navbar>
+    );
+};
+
+export default Header; 
