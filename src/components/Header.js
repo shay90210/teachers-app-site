6eb008822ea5ebfa40ef6822b891422a26c7c33e
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-15 22:10:49 -0500

    Navigation Bar and Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import { Container, Col, Row } from "react-bootstrap";
+import TeacherLogo from '../img/brandLogo2.png';
 
 const Header = () => {
     return (
         <Container>
             <Col>
+                <Row>
+                    <img className='main-logo mt-5' src={TeacherLogo} alt="logo brand" />
+                </Row>
                 <Row>
                     <h1 className='mt-5'>THE TEACHERS' CLUB</h1>
                 </Row>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,19 +1,9 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import TeacherLogo from '../img/brandLogo2.png';
 
 const NavigationBar = () => {
   return (
     <Navbar sticky='top' className='navbar justify-content-center'>
-        <Navbar.Brand href='#home'>
-            <img 
-                src={TeacherLogo} 
-                alt='teacher brand logo' 
-                className='main-logo' 
-                width='100'
-                height='100'
-            />
-        </Navbar.Brand>
         <Nav className='mt-5 mb-5 nav' activeKey='/home'>
             <Nav.Item>
                 <Nav.Link href='/home'>Home</Nav.Link>
