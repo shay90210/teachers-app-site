41c917abcb161904ffcdb13599a8212a9fce8b2b
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-20 20:28:16 -0500

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,11 @@
-import { Container, Col, Row } from "react-bootstrap";
 import TeacherLogo from '../img/brandLogo2.png';
 
 const Header = () => {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <img className='main-logo mt-5' src={TeacherLogo} alt="logo brand" />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h1 className='mt-5'>THE TEACHERS' CLUB</h1>
-                </Col>
-            </Row>
-        </Container>
+        <div>
+            <img className='main-logo mt-5' src={TeacherLogo} alt="logo brand" />
+            <h1 className='mt-5'>THE TEACHERS' CLUB</h1>
+        </div>
     )
 }
 
