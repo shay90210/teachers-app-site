b7224e7a9933c068cef53093999167f3839706f9
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-21 16:15:20 -0500

    Add ClassNames Accordion

--- src/components/AboutAccordion.js
+++ src/components/AboutAccordion.js
@@ -4,14 +4,14 @@ const AboutAccordion = () => {
   return (
     <Accordion>
       <Accordion.Item eventKey="0">
-        <Accordion.Header>MISSION</Accordion.Header>
+        <Accordion.Header className='accord-header'>MISSION</Accordion.Header>
             <Accordion.Body>
                 To provide apparel that vocalizes and celebrates the joys and challenges of
                 teacher life, all while adding some humor.
             </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>VALUES</Accordion.Header>
+        <Accordion.Header className='accord-header'>VALUES</Accordion.Header>
             <Accordion.Body>
                 <p>• Offer stylish swag that links to the latest trends and teacher slang.</p>
                 <p>• Provide a safe space for teachers to freely express themselves.</p>
@@ -20,7 +20,7 @@ const AboutAccordion = () => {
             </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="2">
-        <Accordion.Header>TYPES OF APPAREL</Accordion.Header>
+        <Accordion.Header className='accord-header'>TYPES OF APPAREL</Accordion.Header>
             <Accordion.Body>
                 <p>We offer the following apparel for teachers:</p>
                 <p>• Shirts, Short-Sleeve, and Long-Sleeve</p>
