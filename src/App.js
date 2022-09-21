ddef91ea4de565603105a6b8c5af0d8501a57ae0
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-20 21:27:36 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import NavigationBar from './components/NavigationBar';
-import Header from './components/Header';
-import MainCarousel from './components/MainCarousel';
-import Footer from '../src/components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Homepage';
+import AboutPage from'./pages/Aboutpage';
+import ShopPage from './pages/Shoppage';
+import ContactPage from './pages/Contactpage';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
-      <Header />
-      <MainCarousel />
-      <Footer />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='about' element={<AboutPage />}/>
+        <Route path='podcast' element={<ShopPage />}/>
+        <Route path='contact' element={<ContactPage />}/>
+      </Routes>
     </div>
   );
 }
