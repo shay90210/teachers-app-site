5c867f5e5e93d8c2893a8185ad7a57e0d91f7306
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-22 22:46:00 -0500

    Contact Form

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import HomePage from './pages/Homepage';
 import AboutPage from'./pages/Aboutpage';
 import ShopPage from './pages/Shoppage';
 import ContactPage from './pages/Contactpage';
+import Header from './components/Header';
+import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='about' element={<AboutPage />}/>
-        <Route path='shop' element={<ShopPage />}/>
-        <Route path='contact' element={<ContactPage />}/>
-      </Routes>
+      <NavigationBar />
+      <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='about' element={<AboutPage />}/>
+          <Route path='shop' element={<ShopPage />}/>
+          <Route path='contact' element={<ContactPage />}/>
+        </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Contactpage.js
@@ -0,0 +1,9 @@
+import ContactForm from '../components/ContactForm';
+
+const ContactPage = () => {
+    return (
+        <ContactForm />
+    )
+}
+
+export default ContactPage;
