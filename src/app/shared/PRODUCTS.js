db0104f345baf4978ff96f2e3d1a88022dee8800
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-24 12:12:00 -0500

    Added Product Objects

---
+++ src/app/shared/PRODUCTS.js
@@ -0,0 +1,66 @@
+import cellphonesAwayImg from '../assets/productsimages/cellphonesaway.jpg';
+import espanolShirtImg from '../assets/productsimages/espanolshirt.jpg';
+import ohBecause1Img from '../assets/productsimages/ohbecause1.jpg';
+import ohBecause2Img from '../assets/productsimages/ohbecause2.jpg';
+import ohBecause3Img from '../assets/productsimages/ohbecause3.jpg';
+import readImg from '../assets/productsimages/read.jpg';
+import readuntilImg from '../assets/productsimages/readuntil.jpg';
+import teacherMaskImg from '../assets/productsimages/teachermask.jpg';
+import waterBottleImg from '../assets/productsimages/waterbottle.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 0,
+        name: 'Cellphones Away Shirt',
+        image: cellphonesAwayImg,
+        featured: false,
+    },
+    {
+        id: 1,
+        name: 'Espanol Shirt',
+        image: espanolShirtImg,
+        featured: false,
+    },
+    {
+        id: 2,
+        name: 'Oh Because Grey Sweatshirt',
+        image: ohBecause1Img,
+        featured: false,
+    },
+    {
+        id: 3,
+        name: 'Oh Because Red Sweatshirt',
+        image: ohBecause2Img,
+        featured: false,
+    },
+    {
+        id: 4,
+        name: 'Oh Because Black Sweatshirt',
+        image: ohBecause3Img,
+        featured: false,
+    },
+    {
+        id: 5,
+        name: 'Read Sweatshirt',
+        image: readImg,
+        featured: false,
+    },
+    {
+        id: 6,
+        name: 'Read Until Shirt',
+        image: readuntilImg,
+        featured: false,
+    },
+    {
+        id: 7,
+        name: 'Teacher CDC Approved Mask',
+        image: teacherMaskImg,
+        featured: false,
+    },
+    {
+        id: 8,
+        name: 'Teacher Club Water Bottle',
+        image: waterBottleImg,
+        featured: false,
+    }
+];
