0dd333ca12549dc3e50e49cda901d991b46ce399
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-29 19:42:33 -0500

    React Redux download and index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react';
 import './App.scss';
 import App from './App';
 import 'typeface-cabin';
