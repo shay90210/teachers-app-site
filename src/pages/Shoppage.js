b6dc72ee9d0a0280729eeafe62c4c2035d3c630f
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-29 21:25:11 -0500

    Shop Page Cart Component

--- src/pages/Shoppage.js
+++ src/pages/Shoppage.js
@@ -1,6 +1,27 @@
+import { Component } from "react";
 import { Container } from "react-bootstrap";
+import { Connect } from "react-redux";
+import { Card, CardImg, CardTitle, CardBody, } from "reactstrap";
 
-const ShopPage = () => {
+class ShopPage extends Component {
+    render() {
+        let itemList = this.props.items.map(item=>{
+            return(
+                <div>
+                    <Card key={item.id}>
+                        <CardImg>
+                            <img src={item.img} alt={item.name} />
+                            <CardTitle>{item.title}</CardTitle>
+                        </CardImg>
+
+                        <CardBody>
+                            <p><b>Price: {item.price}$</b></p>
+                        </CardBody>
+                    </Card>
+                </div>
+            )
+            })
+
     return (
         <div className='shop-nav'>
             <Container
@@ -20,9 +41,20 @@ const ShopPage = () => {
 
                     }}
                 > THE TEACHER COLLECTION </p>
+                    <div className="box">
+                        {itemList}
+                    </div>
             </Container>
         </div>
     )
+
+}
 }
 
-export default ShopPage; 
+const mapStateToProps = (state)=>{
+    return {
+      items: state.items
+    }
+  }
+
+export default Connect(mapStateToProps)(ShopPage); 
