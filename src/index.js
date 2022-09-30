38e7e82832639b9a6207211ed1eef6d24144fed2
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-29 23:27:55 -0500

    provider and configure store

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 import React from 'react';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
 import './App.scss';
-import App from './App';
+
 import 'typeface-cabin';
 import 'typeface-open-sans';
 import 'typeface-crafty-girls';
 import 'typeface-indie-flower';
+
 import 'font-awesome/css/font-awesome.css';
 import '@fortawesome/free-brands-svg-icons';
+
 import 'bootstrap-social/bootstrap-social.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const store = configureStore({
+  reducer: {},
+})
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
