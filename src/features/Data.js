5f8e3a773264bd2fcf28e650b6ba5524e6e7a67b
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-29 19:35:43 -0500

    added data

---
+++ src/features/Data.js
@@ -0,0 +1,80 @@
+export const Data = [
+    {
+        id: 0,
+        image: 'readsweatshirt.jpg',
+        name: 'Read Sweatshirt',
+        price: 35
+    },
+        {
+        id: 1,
+        image: 'readuntil1.jpg',
+        name: 'Read Until T Shirt',
+        price: 18
+    },
+        {
+        id: 2, 
+        image: 'mask1.jpg',
+        name: 'CDC Approved Teacher Mask',
+        price: 16
+    },
+        {
+        id: 3,
+        image: 'espanol1.jpg',
+        name: 'Espanol T Shirt',
+        price: 22
+    },
+        {
+        id: 4,
+        image: 'cellphonesaway1.jpg',
+        name: 'Cellphones Away T Shirt',
+        price: 14 
+    },
+        {
+        id: 5,
+        image: 'virtualteachingmug.jpg',
+        name: 'Virtual Teaching Mug',
+        price: 10
+    },
+        {
+        id: 6,
+        image: 'mug1.jpg',
+        name: 'Give Teachers Flowers Mug',
+        price: 10
+    },
+        {
+        id: 7,
+        image: 'ohbeacause1.jpg',
+        name: 'Oh Because... Sweatshirt Red',
+        price: 30
+    },
+        {
+        id: 8,
+        image: 'ohbeacause2.jpg',
+        name: 'Oh Because... Sweatshirt Black',
+        price: 30
+    },
+        {
+        id: 9,
+        image: 'ohbeacause3.jpg',
+        name: 'Oh Because... Sweatshirt Grey',
+        price: 30
+    },
+        {
+        id: 10,
+        image: 'teacherbae1.jpg',
+        name: 'Teacher Bae Red',
+        price: 22
+    },
+        {
+        id: 11,
+        image: 'teacherbae2.jpg',
+        name: 'Teacher Bae White',
+        price: 22
+    },
+        {
+        id: 12,
+        image: 'teacherbae3.jpg',
+        name: 'Teacher Bae Orange',
+        price: 22
+    },
+]
