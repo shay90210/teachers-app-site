955289bcf01c391fdf81ab0e0c69cdd4f7504160
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-29 20:16:17 -0500

    moved cart.js and created cartReducer.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import './App.scss';
 import App from './App';
 import 'typeface-cabin';
@@ -19,11 +17,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
