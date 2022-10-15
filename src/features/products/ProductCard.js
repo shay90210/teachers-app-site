c57b4112d67ae48cd4b97015e322b447fa5a836d
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-14 22:55:44 -0500

    created product card and products list

---
+++ src/features/products/ProductCard.js
@@ -0,0 +1,22 @@
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const ProductCard = ({ product } ) => {
+    const { id, image, name } = ProductCard; 
+    return (
+        <Link to={`${id}`}>
+            <Card>
+                <CardImg 
+                    width='100%'
+                    src={image}
+                    alt={name}
+                    /> 
+                <CardImgOverlay>
+                    <CardTitle>{name}</CardTitle>
+                </CardImgOverlay> 
+            </Card>
+        </Link>
+    );
+};
+
+export default ProductCard; 
---
+++ src/features/products/ProductsList.js
@@ -0,0 +1,21 @@
+import { Col, Row, } from 'reactstrap';
+import { selectAllProducts } from './campsitesSlice';
+import ProductCard from './ProductCard';
+
+const ProductsList = () => {
+    const products = selectAllProducts();
+
+    return (     
+        <Row className="ms-auto">
+            {products.map((product) => {
+                return (
+                    <Col md="5" className="m-4" key={product.id} >
+                        <ProductCard product={product} />
+                    </Col>
+                );
+            })}
+        </Row>  
+    );
+};
+
+export default ProductsList;
