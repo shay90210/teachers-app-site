2d6b1f7485c87dcb1accba8edb6ef3838d9b4dd5
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-14 22:44:13 -0500

    updated products.js

--- src/features/products/products.js
+++ src/features/products/products.js
@@ -1,76 +1,74 @@
-const products = [
+export const products = [
     {
         id: 1,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650992/teachersApp/readsweatshirt_komd7s.jpg',
+        image: 'readsweatshirt.jpg',
         name: 'Read Sweatshirt',
         price: 35
     },
         {
         id: 2, 
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650911/teachersApp/teachermask_awvjcb.jpg',
+        image: 'teachermask.jpg',
         name: 'CDC Approved Teacher Mask',
         price: 16
     },
         {
         id: 3,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650899/teachersApp/espanol1_wlsn3o.jpg',
-        name: 'Espanol T Shirt',
+        image: 'readuntil1.jpg',
+        name: 'Read Until T Shirt',
         price: 22
     },
         {
         id: 4,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650959/teachersApp/cellphonesaway1_szikxl.jpg',
+        image: 'cellphonesaway1.jpg',
         name: 'Cellphones Away T Shirt',
         price: 14 
     },
         {
         id: 5,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650942/teachersApp/virtualteachingmug_hxuay7.jpg',
+        image: 'virtualteachingmug.jpg',
         name: 'Virtual Teaching Mug',
         price: 10
     },
         {
         id: 6,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650980/teachersApp/mug1_fcc1r6.jpg',
+        image: 'mug1.jpg',
         name: 'Give Teachers Flowers Mug',
         price: 10
     },
         {
         id: 7,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650948/teachersApp/ohbeacause1_v7pd2k.jpg',
+        image: 'ohbeacause1.jpg',
         name: 'Oh Because... Sweatshirt Red',
         price: 30
     },
         {
         id: 8,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650949/teachersApp/ohbecause2_x62tqg.jpg',
+        image: 'ohbecause2.jpg',
         name: 'Oh Because... Sweatshirt Black',
         price: 30
     },
         {
         id: 9,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650952/teachersApp/ohbecause3_o0wd7c.jpg',
+        image: 'ohbecause3.jpg',
         name: 'Oh Because... Sweatshirt Grey',
         price: 30
     },
         {
         id: 10,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650938/teachersApp/teacherbae1_hjhtas.jpg',
+        image: 'teacherbae1.jpg',
         name: 'Teacher Bae Red',
         price: 22
     },
         {
         id: 11,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650937/teachersApp/teacherbae2_lv3k9m.jpg',
+        image: 'teacherbae2.jpg',
         name: 'Teacher Bae White',
         price: 22
     },
         {
         id: 12,
-        image: 'https://res.cloudinary.com/dkwk7ox5r/image/upload/v1664650935/teachersApp/teacherbae3_dgpcnr.jpg',
+        image: 'teacherbae3.jpg',
         name: 'Teacher Bae Orange',
         price: 22
     },
 ]
-
-export default products; 
