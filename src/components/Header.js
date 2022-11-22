a7ac372fbf0a53defd221f7ae19783f4543c79e9
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-21 23:20:17 -0600

    adjusted styling and colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,8 @@
 import TeacherLogo from '../img/brandLogo2.png';
-import ChalkboardBG from '../img/chalkboard-1337809_1920.jpg';
 
 const Header = () => {
     return (
-        <div 
-            className='main-header'
-            style={{ 
-                backgroundImage: `url(${ChalkboardBG})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                paddingTop: '50px',
-                paddingBottom: '75px'
-            }}
-        >
+        <div className='main-header'>
             <img className='main-logo mt-5' src={TeacherLogo} alt="logo brand" />
             <h1 className='mt-5'>THE TEACHERS' CLUB</h1>
         </div>
