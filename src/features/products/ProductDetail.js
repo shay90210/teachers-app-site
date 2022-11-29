3d4e17993054258e3b22b3c57659cff6870659da
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-28 20:13:49 -0600

    added an item summary section next to the product detail

--- src/features/products/ProductDetail.js
+++ src/features/products/ProductDetail.js
@@ -1,4 +1,4 @@
-import { Card, CardImg, CardBody, CardText, } 
+import { Card, CardImg, CardBody, CardText, Col, CardTitle } 
 from 'reactstrap';
 import Counter from '../counter/Counter';
 
@@ -7,20 +7,40 @@ const ProductDetail = ( {product} ) => {
 
     return (
         <>
-            <Card className='product-details pt-5 pb-3 m-4'>
-                <CardImg 
-                    top src={image} 
-                    alt={name} 
-                    className='display-image'
-                />
-                <CardBody>
-                    <CardText className='display-name'>{name}</CardText>
-                    <CardText className='display-price'>${price}</CardText>
-                        <div className='counter'>
-                            <Counter /> 
-                        </div>
-                </CardBody>
-            </Card>
+            <Col>
+                <Card className='product-details pt-5 pb-3 m-4'>
+                    <CardImg 
+                        top src={image} 
+                        alt={name} 
+                        className='display-image'
+                    />
+                    <CardBody>
+                        <CardText className='display-name'>{name}</CardText>
+                        <CardText className='display-price'>${price}</CardText>
+                            <div className='counter'>
+                                <Counter /> 
+                            </div>
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col className='mt-4'>
+                <Card>
+                    <CardTitle
+                        style={{
+                            fontSize: '20px',
+                            textAlign: 'center',
+                            fontWeight: 'bold',
+                            marginTop: '10px'
+                        }}
+                    >
+                        Item Summary
+                    </CardTitle>
+                        <hr />
+                    <CardBody>
+
+                    </CardBody>
+                </Card>
+            </Col>
         </>
     );
 };
