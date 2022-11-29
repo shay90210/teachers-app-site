15bdc5adf717b9cb7444265e01c2569a9224d524
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-28 20:56:10 -0600

    updated products slice with create slice from redux

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,4 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { PRODUCTS } from './PRODUCTS';
+
+const initialState = {
+    productsArray: PRODUCTS
+};
+
+const productsSlice = createSlice({
+    name: 'products',
+    initialState
+});
+
+export const productsReducer = productsSlice.reducer;
 
 export const selectAllProducts = () => {
     return PRODUCTS; 
