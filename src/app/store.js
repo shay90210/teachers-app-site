f3e6435ea09c08ae02fc271c6ead999deb00ba49
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-28 20:57:12 -0600

    updated store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { cartReducer } from '../features/reducers/cartReducer';
+import { productsReducer } from '../features/products/productsSlice';
 
 export const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        products: productsReducer
     }
 });
