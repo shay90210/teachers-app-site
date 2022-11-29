0911d8117fbabd86089ba902c7a2784d4b053888
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-28 20:40:45 -0600

    adjusted button styling

--- src/features/products/ProductDetail.js
+++ src/features/products/ProductDetail.js
@@ -5,7 +5,8 @@ import {
     CardText, 
     Col, 
     CardTitle,
-    Button
+    Button,
+    Row
 } 
 from 'reactstrap';
 import Counter from '../counter/Counter';
@@ -15,41 +16,59 @@ const ProductDetail = ( {product} ) => {
 
     return (
         <>
-            <Col>
-                <Card className='product-details pt-5 pb-3 m-4'>
-                    <CardImg 
-                        top src={image} 
-                        alt={name} 
-                        className='display-image'
-                    />
-                    <CardBody>
-                        <CardText className='display-name'>{name}</CardText>
-                        <CardText className='display-price'>${price}</CardText>
-                            <div className='counter'>
-                                <Counter /> 
-                            </div>
-                    </CardBody>
-                </Card>
-            </Col>
-            <Col className='mt-4'>
-                <Card>
-                    <CardTitle
+            <Row>
+                <Col>
+                    <Card className='product-details pt-5 pb-3 m-4'>
+                        <CardImg 
+                            top src={image} 
+                            alt={name} 
+                            className='display-image'
+                        />
+                        <CardBody>
+                            <CardText className='display-name'>{name}</CardText>
+                            <CardText className='display-price'>${price}</CardText>
+                                <div className='counter'>
+                                    <Counter /> 
+                                </div>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col className='mt-4'>
+                    <Card>
+                        <CardTitle
+                            style={{
+                                fontSize: '20px',
+                                textAlign: 'center',
+                                fontWeight: 'bold',
+                                marginTop: '10px'
+                            }}
+                        >
+                            Item Summary
+                        </CardTitle>
+                            <hr />
+                        <CardBody>
+
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='mb-4 mt-4'>
+                    <Button
+                        size='lg'
+                        color='primary'
+                        onClick={() => {
+                            
+                        }}
                         style={{
-                            fontSize: '20px',
-                            textAlign: 'center',
-                            fontWeight: 'bold',
-                            marginTop: '10px'
+                            display: 'block',
+                            margin: 'auto'
                         }}
                     >
-                        Item Summary
-                    </CardTitle>
-                        <hr />
-                    <CardBody>
-
-                    </CardBody>
-                </Card>
-            </Col>
-            <Button>Return to Shop</Button>
+                        Return to Shop
+                    </Button>
+                </Col>
+            </Row>
         </>
     );
 };
